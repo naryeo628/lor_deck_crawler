20f48501f84c2987f789e7a6da086e2e02283ba4
JeongWookLee <naryeo628@naver.com>
2020-07-03 10:48:24 +0900

    make markdown type page

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,16 @@ let codeListService = require('../service/codeListService')
 /* GET home page. */
 router.get('/', function (req, res, next) {
   codeListService.getCodeList().then(data => {
-    res.render('index', { title: 'Express', codeList: data });
+    res.render('index', { title: 'LOR', codeList: data });
   });
 });
+
+
+router.get('/markdown', function (req, res, next) {
+  codeListService.getCodeList().then(data => {
+    res.render('markdown', { title: 'LOR', codeList: data });
+  });
+});
+
 
 module.exports = router;
